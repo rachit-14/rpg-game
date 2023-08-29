82be427e7ab15f11d3484da8c1b38ee7415a4f05
rachit1408 <rachit14082002@gmail.com>
2023-08-29 23:00:06 +0530

    Camera Following Player

--- RPG Project/Assets/Scripts/CameraFollow.cs
+++ RPG Project/Assets/Scripts/CameraFollow.cs
@@ -1,15 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform target;
+    public Vector3 offset;
 
     // Update is called once per frame
     void Update()
-    {   
-        GetComponent<NavMeshAgent>().destination = target.position;
+    {
+        transform.position = target.position + offset;
     }
 }
